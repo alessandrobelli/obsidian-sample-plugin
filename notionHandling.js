e6ea1776765cc190739ca047c0ff4cefae8ec19d
Alessandro Belli <alessandrobelli90@gmail.com>
2023-09-09 15:53:55 +0200

    Update notionHandling.js
    - better code formatting

--- notionHandling.js
+++ notionHandling.js
@@ -5,16 +5,12 @@ import {downloadFile, getImageExtension, writeFilePromise} from "./utilities";
 
 async function getDatabaseName(apiKey, databaseId) {
 	const requestHeaders = {
-		Authorization: `Bearer ${apiKey}`,
-		"Notion-Version": "2022-06-28",
-		"Content-Type": "application/json",
+		Authorization: `Bearer ${apiKey}`, "Notion-Version": "2022-06-28", "Content-Type": "application/json",
 	};
 
 	try {
 		const response = await request({
-			url: `https://api.notion.com/v1/databases/${databaseId}`,
-			method: "GET",
-			headers: requestHeaders,
+			url: `https://api.notion.com/v1/databases/${databaseId}`, method: "GET", headers: requestHeaders,
 		});
 
 		const data = JSON.parse(response);
@@ -28,9 +24,7 @@ async function getDatabaseName(apiKey, databaseId) {
 
 async function fetchNotionData(databaseId, apiKey) {
 	const requestHeaders = {
-		Authorization: `Bearer ${apiKey}`,
-		"Notion-Version": "2022-06-28",
-		"Content-Type": "application/json",
+		Authorization: `Bearer ${apiKey}`, "Notion-Version": "2022-06-28", "Content-Type": "application/json",
 	};
 
 	let results = [];
@@ -58,31 +52,7 @@ async function fetchNotionData(databaseId, apiKey) {
 	return results;
 }
 
-async function extractContentFromPage(pageId, pageName, apiKey, attachmentPath, vaultPath) {
-	const requestHeaders = {
-		Authorization: `Bearer ${apiKey}`,
-		"Notion-Version": "2022-06-28",
-		"Content-Type": "application/json",
-	};
-
-
-	const safeKey = (key) => (/[^\w\s]/.test(key) ? `"${key}"` : key);
-	const safeValue = (value) => (/[\W_]/.test(value) ? `"${value}"` : value);
-
-
-	const response = await request({
-		url: `https://api.notion.com/v1/blocks/${pageId}/children`,
-		method: "GET",
-		headers: requestHeaders,
-	});
-
-	const blocks = JSON.parse(response);
-	console.log('API Response:', JSON.stringify(blocks, null, 2));
-	const promises = [];
-	let content = "";
-	let numberCounter = 1;
-	let fileCounter = 1;
-	let previousBlockType = null;  // Keep track of the previous block type
+async function fetchBlockContent(blocks, previousBlockType, numberCounter, content, attachmentPath, apiKey, pageName, fileCounter, vaultPath, safeKey, promises) {
 	for (const block of blocks.results) {
 		// Reset the numbered list counter if the block type changes
 		if (previousBlockType === 'numbered_list_item' && block.type !== 'numbered_list_item') {
@@ -97,17 +67,15 @@ async function extractContentFromPage(pageId, pageName, apiKey, attachmentPath, 
 				}
 				break;
 			case "paragraph":
-				if (
-					block.paragraph &&
-					block.paragraph.rich_text &&
-					block.paragraph.rich_text.length
-				) {
+				if (block.paragraph && block.paragraph.rich_text && block.paragraph.rich_text.length) {
 					let paragraphContent = "";
 					for (const richTextElement of block.paragraph.rich_text) {
 						if (richTextElement.type === "text") {
 							paragraphContent += richTextElement.plain_text;
 						} else if (richTextElement.type === "mention" && richTextElement.mention.type === "date") {
-							paragraphContent += `@${richTextElement.plain_text}`; // Or however you want to format the date
+							const originalDate = new Date(richTextElement.plain_text);
+							const formattedDate = `${originalDate.getDate()}.${originalDate.getMonth() + 1}.${originalDate.getFullYear()}`;
+							paragraphContent += `[[${formattedDate}]]`; // Date formatted as DD.MM.YYYY
 						} else {
 							// Handle other types as needed
 						}
@@ -119,11 +87,7 @@ async function extractContentFromPage(pageId, pageName, apiKey, attachmentPath, 
 			case "heading_1":
 			case "heading_2":
 			case "heading_3":
-				if (
-					block[block.type] &&
-					block[block.type].rich_text &&
-					block[block.type].rich_text.length
-				) {
+				if (block[block.type] && block[block.type].rich_text && block[block.type].rich_text.length) {
 					const heading = `#`.repeat(Number(block.type.split("_")[1]));
 					let headingContent = "";
 					for (const richTextElement of block[block.type].rich_text) {
@@ -143,11 +107,7 @@ async function extractContentFromPage(pageId, pageName, apiKey, attachmentPath, 
 
 			case "bulleted_list_item":
 			case "numbered_list_item":
-				if (
-					block[block.type] &&
-					block[block.type].rich_text &&
-					block[block.type].rich_text.length
-				) {
+				if (block[block.type] && block[block.type].rich_text && block[block.type].rich_text.length) {
 					const prefix = block.type === "bulleted_list_item" ? "-" : `${numberCounter}.`;
 
 					// Increment the number counter if it's a numbered list item
@@ -180,46 +140,42 @@ async function extractContentFromPage(pageId, pageName, apiKey, attachmentPath, 
 				if (block.image && block.image.external && block.image.external.url) {
 					const imageUrl = block.image.external.url;
 					const fileExtension = getImageExtension(imageUrl);
-					const imagePath = path.join(
-						attachmentPath, // Use attachmentPath here
-						`image_${Date.now()}.${fileExtension}`
-					);
+					const imagePath = path.join(attachmentPath, // Use attachmentPath here
+						`image_${Date.now()}.${fileExtension}`);
 					await downloadFile(imageUrl, imagePath, app, apiKey); // Assuming app and apiKey are available
 					content += `![[${path.basename(imagePath)}]]\n\n`;
 				}
 				break;
 			case "to_do":
-				if (
-					block.to_do &&
-					block.to_do.text &&
-					block.to_do.text.length
-				) {
+				if (block.to_do && block.to_do.rich_text && block.to_do.rich_text.length) {
 					const checkbox = block.to_do.checked ? "[x]" : "[ ]";
-					content += `${checkbox} ${block.to_do.text
-						.map((t) => t.plain_text)
-						.join("")}\n`;
+					let todoContent = "";
+					for (const richTextElement of block.to_do.rich_text) {
+						let textContent = richTextElement.plain_text;
+						if (richTextElement.annotations.bold) {
+							textContent = `**${textContent}**`;
+						}
+						if (richTextElement.annotations.italic) {
+							textContent = `*${textContent}*`;
+						}
+						todoContent += textContent;
+					}
+					content += `${checkbox} ${todoContent}\n`;
 				}
 				break;
 			case "table":
 				if (block.table && block.table.headers && block.table.rows) {
 					content += "| " + block.table.headers.join(" | ") + " |\n";
-					content +=
-						"| " +
-						new Array(block.table.headers.length)
-							.fill("---")
-							.join(" | ") +
-						" |\n";
+					content += "| " + new Array(block.table.headers.length)
+						.fill("---")
+						.join(" | ") + " |\n";
 					block.table.rows.forEach((row) => {
 						content += "| " + row.join(" | ") + " |\n";
 					});
 				}
 				break;
 			case "code":
-				if (
-					block.code &&
-					block.code.rich_text &&
-					block.code.rich_text.length
-				) {
+				if (block.code && block.code.rich_text && block.code.rich_text.length) {
 					let codeContent = block.code.rich_text.map((text) => text.plain_text).join("");
 					const language = block.code.language ? block.code.language : "";
 					content += `\`\`\`${language}\n${codeContent}\n\`\`\`\n\n`;
@@ -233,15 +189,30 @@ async function extractContentFromPage(pageId, pageName, apiKey, attachmentPath, 
 				}
 				break;
 
+			case "toggle":
+				if (block.toggle && block.toggle.rich_text && block.toggle.rich_text.length) {
+					let toggleContent = "";
+					for (const richTextElement of block.toggle.rich_text) {
+						let textContent = richTextElement.plain_text;
+						toggleContent += textContent;
+					}
+					content += `> [!NOTE]+ ${toggleContent} \n`;
+				}
+				break;
+
+			case "video":
+				if (block.video && block.video.type === "external" && block.video.external.url) {
+					const videoUrl = block.video.external.url;
+					content += `Video: [${videoUrl}](${videoUrl})\n\n`;
+				}
+				break;
 			case "audio":
 				if (block.audio && block.audio.file && block.audio.file.url) {
 					const audioUrl = block.audio.file.url;
 					const audioExtension = path.extname(new URL(audioUrl).pathname);
 					const audioFileName = `${pageName}_${fileCounter}${audioExtension}`;
-					const audioFilePath = path.join(
-						attachmentPath, // Use attachmentPath here
-						audioFileName
-					);
+					const audioFilePath = path.join(attachmentPath, // Use attachmentPath here
+						audioFileName);
 					await downloadFile(audioUrl, audioFilePath, app, apiKey);  // Assuming app and apiKey are available
 
 					content += `![[${path.basename(audioFilePath)}]]\n\n`;  // Or however you want to reference the audio file
@@ -255,10 +226,7 @@ async function extractContentFromPage(pageId, pageName, apiKey, attachmentPath, 
 					const fileUrl = block.file.file.url;
 					const fileExtension = path.extname(new URL(fileUrl).pathname);
 					const fileName = `${pageName}_${fileCounter}${fileExtension}`;
-					const filePath = path.join(
-						attachmentPath,
-						fileName
-					);
+					const filePath = path.join(attachmentPath, fileName);
 					await downloadFile(fileUrl, filePath, app, apiKey);
 
 					content += `![[${path.basename(filePath)}]]\n\n`;
@@ -304,15 +272,64 @@ async function extractContentFromPage(pageId, pageName, apiKey, attachmentPath, 
 		// Update the previous block type
 		previousBlockType = block.type;
 
+		if (block.has_children) {
+			console.log(`=== Fetching children for block ID: ${block.id} ===`);  // Very visible console.log message
 
+			// Fetch children of the block
+			const childBlocksResponse = await request({
+				url: `https://api.notion.com/v1/blocks/${block.id}/children`,
+				method: "GET",
+				headers: {
+					'Authorization': `Bearer ${apiKey}`,
+					'Notion-Version': '2022-06-28'
+				}
+			});
+			const childBlocks = JSON.parse(childBlocksResponse);
+
+			// Recursively get the content for child blocks
+			const childContent = await fetchBlockContent({results: childBlocks.results}, previousBlockType, numberCounter, "", attachmentPath, apiKey, pageName, fileCounter, vaultPath, safeKey, promises);
+
+			// Add ">" at the start of each line if the block is a "toggle" type
+			if (block.type === "toggle") {
+				const indentedChildContent = childContent.split("\n").map(line => "> " + line).join("\n");
+				content += indentedChildContent;
+			} else {
+				content += childContent;
+			}
+		}
 	}
+
+
+	return content;
+}
+
+async function extractContentFromPage(pageId, pageName, apiKey, attachmentPath, vaultPath) {
+	const requestHeaders = {
+		Authorization: `Bearer ${apiKey}`, "Notion-Version": "2022-06-28", "Content-Type": "application/json",
+	};
+
+
+	const safeKey = (key) => (/[^\w\s]/.test(key) ? `"${key}"` : key);
+	const safeValue = (value) => (/[\W_]/.test(value) ? `"${value}"` : value);
+
+
+	const response = await request({
+		url: `https://api.notion.com/v1/blocks/${pageId}/children`, method: "GET", headers: requestHeaders,
+	});
+
+	const blocks = JSON.parse(response);
+	console.log('API Response:', JSON.stringify(blocks, null, 2));
+	const promises = [];
+	let content = "";
+	let numberCounter = 1;
+	let fileCounter = 1;
+	let previousBlockType = null;  // Keep track of the previous block type
+	content = await fetchBlockContent(blocks, previousBlockType, numberCounter, content, attachmentPath, apiKey, pageName, fileCounter, vaultPath, safeKey, promises);
 
 	await Promise.all(promises);
 	return content;
 }
 
 module.exports = {
-	fetchNotionData,
-	extractContentFromPage,
-	getDatabaseName
+	fetchNotionData, extractContentFromPage, getDatabaseName
 };
